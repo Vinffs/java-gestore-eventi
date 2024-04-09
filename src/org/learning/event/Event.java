1827aaa47f64e73eb87c12560fd901c4ee5e47b4
Vinffs <vin.demiranda@gmail.com>
2024-04-09 10:05:15 +0200

    created methods to makeReservation, cancelReservation and Override of toString

--- src/org/learning/event/Event.java
+++ src/org/learning/event/Event.java
@@ -39,6 +39,33 @@ public class Event {
         return totalSeats;
     }
 
+    public void makeReservation(int seatsToBook) throws IllegalArgumentException {
+        if (date.isBefore(LocalDate.now())) {
+            throw new IllegalArgumentException("You can't book an event that is in the past.");
+        }
+        if (seatsBooked + seatsToBook > totalSeats) {
+            throw new IllegalArgumentException("There aren't enough available places to proceed with your reservation.");
+        }
+        seatsBooked += seatsToBook;
+    }
+
+    public void cancelReservation(int seatsToCancel) throws IllegalArgumentException {
+        if (date.isBefore(LocalDate.now())) {
+            throw new IllegalArgumentException("You can't cancel a reservation of an event that is in the past.");
+        }
+        if (seatsToCancel > seatsBooked) {
+            throw new IllegalArgumentException("The number of seats to cancel is superior to the number of " +
+                    "reservations.");
+        }
+        seatsBooked -= seatsToCancel;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s - %s", date.toString(), title);
+    }
+
+
     // Getters
     public String getTitle() {
         return title;
