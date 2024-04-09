d50f64231759d61ced4c9c6cc5b68520222b6b06
Vinffs <vin.demiranda@gmail.com>
2024-04-09 10:25:52 +0200

    ask user for a reservation, calling Event methods and using try catch to handle errors

--- src/org/learning/event/Main.java
+++ src/org/learning/event/Main.java
@@ -20,6 +20,28 @@ public class Main {
 
         Event event = new Event(title, date, totalSeats);
 
+        System.out.println("\n" + "Your Event has been created successfully!");
+        System.out.println();
+
+
+        System.out.print("Do you want to make a reservation? (Y/N) ");
+        String response = scan.nextLine().toUpperCase();
+        while (response.equalsIgnoreCase("Y")) {
+            try {
+                System.out.print("How many seats do you want to book? ");
+                int seatsToBook = Integer.parseInt(scan.nextLine());
+                event.makeReservation(seatsToBook);
+                System.out.println("Reservation made successfully!");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+            System.out.print("Do you want to make another reservation? (Y/N) ");
+            response = scan.nextLine().toUpperCase();
+        }
+
+
+        System.out.println("Booked seats: " + event.getSeatsBooked());
+        System.out.println("Available seats: " + (event.getTotalSeats() - event.getSeatsBooked()));
 
         scan.close();
     }
