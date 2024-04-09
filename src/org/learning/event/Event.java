65fbfc0f9a229fce747f1c07b34c37ca96dac61e
Vinffs <vin.demiranda@gmail.com>
2024-04-09 10:03:11 +0200

    created constructor in Event

--- src/org/learning/event/Event.java
+++ src/org/learning/event/Event.java
@@ -15,4 +15,29 @@ public class Event {
     private int seatsBooked;
 
 
+    // Constructor
+
+    public Event(String title, LocalDate date, int totalSeats) {
+        this.title = title;
+        this.date = checkDate(date);
+        this.totalSeats = checkCapacity(totalSeats);
+        this.seatsBooked = 0;
+    }
+
+    // Methods
+    private LocalDate checkDate(LocalDate date) throws IllegalArgumentException {
+        if (date.isBefore(LocalDate.now()) ) {
+            throw new IllegalArgumentException("You can't insert an event which date is in the past");
+        }
+        return date;
+    }
+
+    private int checkCapacity(int totalSeats) throws IllegalArgumentException {
+        if (totalSeats <= 0) {
+            throw new IllegalArgumentException("You can't insert a number <= 0");
+        }
+        return totalSeats;
+    }
+
+
 }
