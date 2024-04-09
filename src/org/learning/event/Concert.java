7936a48fa766f0c4e0ceecd41d80b21f5e47bb4f
Vinffs <vin.demiranda@gmail.com>
2024-04-09 12:29:56 +0200

    Created Concert class , with it's attributes and constructor ( extends Event )

---
+++ src/org/learning/event/Concert.java
@@ -0,0 +1,21 @@
+package org.learning.event;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class Concert extends Event{
+
+    //Attributes
+    LocalTime time;
+    BigDecimal price;
+
+    // Constructor
+    public Concert(String title, LocalDate date, int totalSeats, LocalTime time, BigDecimal price) {
+        super(title, date, totalSeats);
+        this.time = time;
+        this.price = price;
+    }
+
+}
