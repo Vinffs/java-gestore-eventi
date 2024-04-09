129f1d8205dded83fe917c5b5f2062d136dc977e
Vinffs <vin.demiranda@gmail.com>
2024-04-09 10:02:04 +0200

    created Event and Main and added attributes

---
+++ src/org/learning/event/Event.java
@@ -0,0 +1,18 @@
+package org.learning.event;
+
+import java.time.LocalDate;
+
+public class Event {
+
+    // Attributes
+
+    private String title;
+
+    private LocalDate date;
+
+    private int totalSeats;
+
+    private int seatsBooked;
+
+
+}
---
+++ src/org/learning/event/Main.java
@@ -0,0 +1,7 @@
+package org.learning.event;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
