b9c6e7cd5bd95b7de3ac6ad51c860d064efead12
Vinffs <vin.demiranda@gmail.com>
2024-04-09 14:59:43 +0200

    testing Concert class

--- src/org/learning/event/Concert.java
+++ src/org/learning/event/Concert.java
@@ -58,4 +58,26 @@ public class Concert extends Event{
     public void setPrice(BigDecimal price) {
         this.price = price;
     }
+
+
+    // Main used for Concert Testing
+
+    public static void main(String[] args) {
+
+        Concert concertOne = new Concert("Rock Concert", LocalDate.parse("2024-10-01"), 200,
+                LocalTime.parse("20:30:00"), new BigDecimal("19.99"));
+
+        Concert concertTwo = new Concert("Pop Concert", LocalDate.parse("2024-09-01"), 250, LocalTime.parse("19:20:50"),
+                new BigDecimal("29.99"));
+
+        Concert concertThree = new Concert("Metal Concert", LocalDate.parse("2024-12-01"), 900, LocalTime.parse("22" +
+                ":00:20"), new BigDecimal("39.99"));
+
+
+        System.out.println(concertOne.toString());
+        System.out.println(concertTwo.toString());
+        System.out.println(concertThree.toString());
+
+
+    }
 }
